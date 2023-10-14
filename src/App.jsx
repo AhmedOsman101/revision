58b4fb5b4f9a99b5ec091070f6dcf0494d7c0a77
AhmedOsman101 <trollingosman101@outlook.com>
2023-10-14 17:44:15 +0300

    setting things up

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import React, {
+	Component,
+	useContext,
+	useEffect,
+	useReducer,
+	useState,
+} from "react";
+import logo from "./logo.svg";
+import axios from "axios";
+import { Routes, Route } from "react-router-dom";
+
+export default function App() {
+	return (
+		<>
+			<h1>hello from app</h1>
+		</>
+	);
+}
