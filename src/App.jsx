9e87bd2b9b361bfbdd3d4b5d37608bfb0ba318b1
AhmedOsman101 <trollingosman101@outlook.com>
2023-10-14 18:10:18 +0300

    finished Map method and ternary operators

--- src/App.jsx
+++ src/App.jsx
@@ -8,11 +8,12 @@ import React, {
 import logo from "./logo.svg";
 import axios from "axios";
 import { Routes, Route } from "react-router-dom";
+import ClassMap from "./Components/ClassMap";
 
 export default function App() {
 	return (
 		<>
-			<h1>hello from app.js</h1>
+			<ClassMap />
 		</>
 	);
 }
---
+++ src/Components/ClassMap.jsx
@@ -0,0 +1,23 @@
+import React, { Component } from "react";
+
+class ClassMap extends Component {
+	render() {
+		const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+		console.log(arr);
+		return (
+			<>
+				<div>
+					{arr.map((item) => {
+						return (
+							<>
+								<div>{item % 2 === 0 ? item * item : item}</div>
+							</>
+						);
+					})}
+				</div>
+			</>
+		);
+	}
+}
+
+export default ClassMap;
