230a7ab7ef248e16215354a8957c76e8f7b1decd
AhmedOsman101 <trollingosman101@outlook.com>
2023-10-14 22:08:40 +0300

    formatting

--- src/Components/FuncFilter.jsx
+++ src/Components/FuncFilter.jsx
@@ -1,9 +1,10 @@
+/* eslint-disable no-unused-vars */
 import React from "react";
 
 export default function FuncFilter() {
 	const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 	const newArr = arr.filter((item, index) => {
-		return item % 2 != 0;
+		return item % 2 !== 0;
 	});
-    console.log(newArr);
+	console.log(newArr);
 }
