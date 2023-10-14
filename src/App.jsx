9dea284a11e21717e8d6b819cd39c2ab0eb215f8
AhmedOsman101 <trollingosman101@outlook.com>
2023-10-14 18:30:19 +0300

    finished Reduce method

--- src/App.jsx
+++ src/App.jsx
@@ -9,13 +9,15 @@ import logo from "./logo.svg";
 import axios from "axios";
 import { Routes, Route } from "react-router-dom";
 import ClassMap from "./Components/ClassMap";
+import ClassReduce from "./Components/ClassReduce";
 import FuncFilter from "./Components/FuncFilter";
 
 export default function App() {
 	return (
 		<>
 			{/* <ClassMap /> */}
-			<FuncFilter />
+			{/* <FuncFilter /> */}
+			<ClassReduce />
 		</>
 	);
 }
---
+++ src/Components/ClassReduce.jsx
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+
+export default class ClassReduce extends Component {
+	constructor(props) {
+		super(props);
+		const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+		const newArr = arr.reduce((prev, value) => {
+			let result = prev + value;
+			return result;
+		});
+		console.log(newArr);
+	}
+
+	render() {
+		return <>j</>;
+	}
+}
